736acb6048b06f205ab876eaa7c553f0d6c97922
Hugo Miranda Alves <hugo.miranda@FAJ.EG.AD>
2025-02-19 10:35:22 -0300

    Apaguei status bar

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './src/components/Title/';
 import Form from './src/components/Form/';
